eb7f4f8df8f370374bab8212db12ff09b57799d6
Heroin <heroin.nee@gmail.com>
2013-02-22 20:04:12 +0800

    format

--- go-upload-server/src/go-upload-server.go
+++ go-upload-server/src/go-upload-server.go
@@ -13,8 +13,8 @@ const (
 	PORT      = 12322
 	DOWNLOAD  = "http://127.0.0.1/%s"
 	LOCAL_URL = "/www/%s"
-	INDEX = `<!DOCTYPE html><html><head><title>UPLOAD</title></head><body><form action="/upload" method="post" enctype ="multipart/form-data"><input name="file" type="file"/><button>upload</button></form></body></html>`
-	UPLOAD = `<!DOCTYPE html><html><head><title>UPLOAD</title></head><body><a href="%s">%s</a><br/><a href="http://192.168.192.81">Home</a></body></html>`
+	INDEX     = `<!DOCTYPE html><html><head><title>UPLOAD</title></head><body><form action="/upload" method="post" enctype ="multipart/form-data"><input name="file" type="file"/><button>upload</button></form></body></html>`
+	UPLOAD    = `<!DOCTYPE html><html><head><title>UPLOAD</title></head><body><a href="%s">%s</a><br/><a href="http://192.168.192.81">Home</a></body></html>`
 )
 
 func index(out http.ResponseWriter, request *http.Request) {
--- go-wget-server/src/go-wget-server.go
+++ go-wget-server/src/go-wget-server.go
@@ -1,57 +1,57 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "log"
-    "os/exec"
-    "runtime"
+	"fmt"
+	"log"
+	"net/http"
+	"os/exec"
+	"runtime"
 )
 
 const (
-    DOWNLOAD_PATH = "/www/domain/download.heroin.so/"
-    SERVER = "GO-WGET-SERVER"
-    PORT = 12321
+	DOWNLOAD_PATH = "/www/domain/download.heroin.so/"
+	SERVER        = "GO-WGET-SERVER"
+	PORT          = 12321
 )
 
 func download(path string) {
-    runtime.Gosched()
-    log.Printf("download start %s \n", path)
-    cmd := exec.Command("wget.exe", "-P", DOWNLOAD_PATH, path)
-    err := cmd.Run()
-    if err != nil {
-        log.Printf("download [error] path=%s, ", path, err)
-    }
-    runtime.GC()
-    log.Printf("download over %s \n", path)
+	runtime.Gosched()
+	log.Printf("download start %s \n", path)
+	cmd := exec.Command("wget.exe", "-P", DOWNLOAD_PATH, path)
+	err := cmd.Run()
+	if err != nil {
+		log.Printf("download [error] path=%s, ", path, err)
+	}
+	runtime.GC()
+	log.Printf("download over %s \n", path)
 }
 
 func index(out http.ResponseWriter, request *http.Request) {
-    out.Header().Set("Server", SERVER)
+	out.Header().Set("Server", SERVER)
 
-    if (request.URL.Path != "/" && request.URL.Path != "/favicon.ico") {
-        go download(fmt.Sprintf("http:/%s", request.URL.Path))
-        fmt.Fprintf(out, "download http:/" + request.URL.Path + "\r\n")
-    } else {
-        fmt.Fprintf(out, "error\r\n")
-    }
+	if request.URL.Path != "/" && request.URL.Path != "/favicon.ico" {
+		go download(fmt.Sprintf("http:/%s", request.URL.Path))
+		fmt.Fprintf(out, "download http:/"+request.URL.Path+"\r\n")
+	} else {
+		fmt.Fprintf(out, "error\r\n")
+	}
 }
 
 func remove(out http.ResponseWriter, request *http.Request) {
-    out.Header().Set("Server", SERVER)
+	out.Header().Set("Server", SERVER)
 
-    request.ParseForm()
-    fmt.Println(request.Form["file"])
-    fmt.Println(request.Form["dir"])
-    fmt.Fprintf(out, "rm\r\n")
+	request.ParseForm()
+	fmt.Println(request.Form["file"])
+	fmt.Println(request.Form["dir"])
+	fmt.Fprintf(out, "rm\r\n")
 }
 
 func main() {
-    runtime.GOMAXPROCS(5)
-    http.HandleFunc("/", index)
-    http.HandleFunc("/rm", remove)
-    err := http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	runtime.GOMAXPROCS(5)
+	http.HandleFunc("/", index)
+	http.HandleFunc("/rm", remove)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
